ae65f4b78481fe882ce493db0774df632dce1859
DavidB954 <david_o_bellido@hotmail.com>
2026-04-29 23:39:32 -0300

    feat(bitacora): implementación inicial de filtros (en desarrollo)

--- TurnosMedicos/BE/BE_Usuario.cs
+++ TurnosMedicos/BE/BE_Usuario.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,6 +24,8 @@ namespace BE
         public int IntentosFallidos { get; set; }   
 
         public bool Activo { get; set; }
+
+        public string NombreApellido { get { return Nombre + " " + Apellido; } }
 
     }
 }
---
+++ TurnosMedicos/GestionTurnosMedicos/ucCboUsuario.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace GestionTurnosMedicos
+{
+    partial class ucCboUsuario
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboUsuario = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cboUsuario
+            // 
+            this.cboUsuario.FormattingEnabled = true;
+            this.cboUsuario.Location = new System.Drawing.Point(3, 3);
+            this.cboUsuario.Name = "cboUsuario";
+            this.cboUsuario.Size = new System.Drawing.Size(97, 21);
+            this.cboUsuario.TabIndex = 0;
+            // 
+            // ucCboUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cboUsuario);
+            this.Name = "ucCboUsuario";
+            this.Size = new System.Drawing.Size(110, 29);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboUsuario;
+    }
+}
